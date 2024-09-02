17859b0103bca59cdf55fd757c94ac8fb3748e93
AdamGhirvanF <adam_ghirvan@yahoo.co.id>
2024-09-02 11:06:41 +0700

    new branch

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello Adam Ghirvan Fadhil" }');
+    res.send('{ "response": "Hello Adam Ghirvan Fadhil Master" }');
 });
 
 app.get('/will', function (req, res) {
